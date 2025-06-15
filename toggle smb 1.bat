ba5e6fddfe030ec556aaecae8eac30a3a98520a3
carnferg01 <carnferg01@gmail.com>
2025-06-15 15:44:07 +0100

    Inital commit.

---
+++ toggle smb 1.bat
@@ -0,0 +1,71 @@
+::::::::::::::::::::::::::::::::::::::::::::
+:: Automatically check & get admin rights V2
+::::::::::::::::::::::::::::::::::::::::::::
+@echo off
+CLS
+ECHO.
+ECHO =============================
+ECHO Running Admin shell
+ECHO =============================
+
+:init
+setlocal DisableDelayedExpansion
+set "batchPath=%~0"
+for %%k in (%0) do set batchName=%%~nk
+set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
+setlocal EnableDelayedExpansion
+
+:checkPrivileges
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
+ECHO.
+ECHO **************************************
+ECHO Invoking UAC for Privilege Escalation
+ECHO **************************************
+
+ECHO Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
+ECHO args = "ELEV " >> "%vbsGetPrivileges%"
+ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
+ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
+ECHO Next >> "%vbsGetPrivileges%"
+ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+"%SystemRoot%\System32\WScript.exe" "%vbsGetPrivileges%" %*
+exit /B
+
+:gotPrivileges
+setlocal & pushd .
+cd /d %~dp0
+if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
+
+::::::::::::::::::::::::::::
+::START
+::::::::::::::::::::::::::::
+
+Dism /online /Get-Features /format:table | find "SMB1Protocol"
+
+echo.
+set /p id="  y to turn on, n to turn off and then enter: "
+if "%id%"=="y" goto on
+if "%id%"=="n" goto off
+
+cls
+echo.
+echo --- Nothing changed ---
+goto end
+
+:on
+cls
+Dism /online /Enable-Feature /FeatureName:"SMB1Protocol" -All
+goto end
+ 
+:off
+cls
+Dism /online /Disable-Feature /FeatureName:"SMB1Protocol"
+goto end
+
+:end
+echo.
+pause
